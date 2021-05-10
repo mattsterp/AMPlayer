0744f3d6a5ff95e879b3493f7b83a29c2e98f326
Matthhew Pters <mattsterp@gmail.com>
2021-05-10 11:50:36 -0400

    Initial Commit

---
+++ Shared/AMPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AMPlayerApp.swift
+//  Shared
+//
+//  Created by Matthew Peters on 5/10/21.
+//
+
+import SwiftUI
+
+@main
+struct AMPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shared/View/Home.swift
@@ -0,0 +1,28 @@
+//
+//  Home.swift
+//  AMPlayer (iOS)
+//
+//  Created by Matthew Peters on 5/10/21.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View{
+
+        HStack(spacing: 0){
+            
+            // Side Tab Bar
+            VStack{
+                
+            }
+            // Max side sidebar width...
+            .frame(width: 80)
+        }
+    }
+}
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
